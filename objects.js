1e2ab0b7480f0d2ec134eafd0d7c9a47a527e337
Joe Quattrone <joequattrone5@gmail.com>
2018-10-04 18:24:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {artistName: "songTitle",}
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return  playlist[artistName] = songTitle )
+  return  playlist[artistName] = songTitle }
 }
 
 updatePlaylist(PhillOchs, "Phil Ochs", "Slowdive"); 
